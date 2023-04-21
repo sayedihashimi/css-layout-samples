c5ac8b53cc804ebe1d89c2323663b9b6922f203d
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2023-04-20 23:31:07 -0400

    updating the horizontal tabs

--- html/horizontal-tabs/index.js
+++ html/horizontal-tabs/index.js
@@ -1,22 +1,30 @@
+//hljs.initHighlightingOnLoad();
 window.onload = (event) => {
-
     // register click handlers for the tab headers
     const tabHeaders = document.querySelectorAll(".navLink");
     tabHeaders.forEach( (th)=>{
         th.addEventListener("click", handleOnClick, false);
     });
+
+    hljs.highlightAll();
+    hljs.highlightElement(document.querySelector("#rawResult"));
+    // this prevents a flicker on the content after styles are applied
+    document.querySelector('.content').removeAttribute('hidden');
 };
 
 function handleOnClick(event){
-    console.log('clicked');
     // hide all the content and then show the selected one
     const contentElements = document.querySelectorAll(".tabContent");
     contentElements.forEach( (element)=>{
-        console.log('here');
         element.setAttribute('hidden','');
     });
-
+    const tabHeaderElements = document.querySelectorAll(".navLink");
+    tabHeaderElements.forEach( (element)=>{
+        element.removeAttribute('current');
+    });
+    
     document.querySelector(event.currentTarget.getAttribute('href')).removeAttribute('hidden');
 
+    event.currentTarget.setAttribute('current','');
     event.preventDefault();
 }
